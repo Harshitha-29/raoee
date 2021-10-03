5da7337919886ddd41bee8b8ad0a6415c4449b10
reetikgit <reetikchitragupt@gmail.com>
2021-10-03 16:51:15 +0530

    dfdf

--- public/employee/user1.js
+++ public/employee/user1.js
@@ -6,11 +6,29 @@ const storage = firebase.storage();
 let USER = false;
 let USER_REF = false;
 let USER_ID = false;
-
+var userDetails;
 auth.onAuthStateChanged((user) => {
   USER_ID = user.uid;
+  userDetails = user
   getUserDetails({ uid: user.uid, userType: user.displayName });
+  console.log(user)
+  if(user.emailVerified==false){
+    
+     $('#exampleModalCenter').modal({backdrop: 'static', keyboard: false,show:true}) 
+     document.getElementById("emailID").innerHTML=user.email
+    
+    
+  }
 });
+////To Send Email again/////
+function sendEmail(){
+  console.log(userDetails)
+   userDetails.sendEmailVerification().then(function() {
+
+    nowuiDashboard.showNotification('top','center',"Email Sent Successfully","primary");
+  })
+}
+
 // ////////////////////////////////////////////
 let retryUser = 0;
 async function getUserDetails({ uid, userType }) {
