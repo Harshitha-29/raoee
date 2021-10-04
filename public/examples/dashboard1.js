5226c47d36228fb021c60c70d643b1d595924f95
reetikgit <reetikchitragupt@gmail.com>
2021-10-04 19:16:46 +0530

    dfdf

--- public/examples/dashboard1.js
+++ public/examples/dashboard1.js
@@ -1,3 +1,5 @@
+
+
 const db = firebase.firestore();
 const auth = firebase.auth();
 const storage = firebase.storage();
@@ -28,3 +30,64 @@ function sendEmail(){
     nowuiDashboard.showNotification('top','center',"Email Sent Successfully","primary");
   })
 }
+
+db.collection("sliders").onSnapshot((snaps) => {
+    const docs = snaps.docs;
+    let trow = ``;
+    document.getElementById("slidersData").innerHTML="" 
+    docs.map((doc, index) => {
+      let docData = doc.data();
+       console.log(docData.url)
+        title="";
+        if(docData.title){
+           title = docData.title; 
+        }
+  //     console.log(docData +" "+)
+        if(index==0){
+            document.getElementById("slidersData").innerHTML += `
+
+                <div class="carousel-item active">
+                    <img
+                        class="d-block center-image"
+                        src="`+docData.url+`"
+                        alt="First slide"
+                    />
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5 class="slider-txt">`+title+`</h5>
+                        <p class="slidersm-txt">Here</p>
+                    </div>
+                </div>
+                ` 
+        }else{
+            document.getElementById("slidersData").innerHTML += `
+
+                <div class="carousel-item ">
+                    <img
+                        class="d-block center-image"
+                        src="`+docData.url+`"
+                        alt="First slide"
+                    />
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5 class="slider-txt">`+docData.title+`</h5>
+                        <p class="slidersm-txt">Here</p>
+                    </div>
+                </div>
+                ` 
+        }
+       
+    });
+
+  });
+
+  db.collection("miscellaneous").onSnapshot((snaps) => {
+
+    const docs = snaps.docs;
+
+    docs.map((doc, index) => {
+        let docData = doc.data();
+
+        if(docData.aboutUs){
+            document.getElementById("aboutTxt").innerHTML= docData.aboutUs
+        }
+    })
+  });
